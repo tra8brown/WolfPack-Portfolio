44580290b0c48e25eabe6dd9dccb2579f5a77d31
TraBee <tra8brown@gmail.com>
2021-09-19 01:31:54 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from "react-router-dom";
+import Portfolio from "./components/Portfolio";
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume"
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        BrowserRouter >
+        <
+        Switch >
+        <
+        Route component = { Portfolio }
+        path = '/'
+        exact / >
+        <
+        Route component = { AboutMe }
+        path = '/AboutMe' / >
+        <
+        Route component = { Contact }
+        path = '/Contact' / >
+        <
+        Route component = { Resume }
+        path = '/Resume' / >
+        <
+        /Switch>  <
+        /BrowserRouter> 
+    )
 }
 
 export default App;
