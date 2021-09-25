b051b2e9b2b866c3e6f3ad6c7fdabcd3f7ecea06
TraBee <tra8brown@gmail.com>
2021-09-24 23:45:44 -0600

    rendering page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import {
-    BrowserRouter,
-    Route,
-    Switch
-} from "react-router-dom";
-import Portfolio from "./components/Portfolio";
-import AboutMe from "./components/AboutMe";
-import Contact from "./components/Contact";
-import Resume from "./components/Resume";
-import NavBar from './components/NavBar';
-//import logo from './logo.svg';
-//import './App.css';
+import React from 'react';
+import './App.css';
+import AboutMe from './components/AboutMe';
 
-function App() {
-    return ( <BrowserRouter >
-        <NavBar / >
-        <Switch >
-        <Route component = { Portfolio }path = '/'exact / >
-        <Route component = { AboutMe }path = '/AboutMe' / >
-        <Route component = { Contact }path = '/Contact' / >
-        <Route component = { Resume }path = '/Resume' / >
-        </Switch>    
-        </BrowserRouter>
-    )
+function app () {
+    return(
+<AboutMe />
+    );
 }
 
-export default App;
+
+// import {
+//     BrowserRouter,
+//     Route,
+//     Switch
+// } from "react-router-dom";
+// import Portfolio from "./components/Portfolio";
+// import AboutMe from "./components/AboutMe";
+// import Contact from "./components/Contact";
+// import Resume from "./components/Resume";
+// import NavBar from './components/NavBar';
+// //import logo from './logo.svg';
+// //import './App.css';
+
+// function App() {
+//     return ( <BrowserRouter >
+//         <NavBar / >
+//         <Switch >
+//         <Route component = { Portfolio }path = '/'exact / >
+//         <Route component = { AboutMe }path = '/AboutMe' / >
+//         <Route component = { Contact }path = '/Contact' / >
+//         <Route component = { Resume }path = '/Resume' / >
+//         </Switch>    
+//         </BrowserRouter>
+//     )
+// }
+
+export default app;
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,15 @@
-import React from "react"
+import React from 'react';
 
-export default function About() {
-    return <h1> About Me Page! </h1>
+
+const AboutMe = ()=>{
+    return(
+        <div>
+            <h1>About Me</h1>
+            <p>I am currently a student at University of Utah, taking a Full Stack Web Developing Bootcamp course. The past 13 years I have enjoyed being a stay-at-home mother and since my kids are growing and have started school, I decided now would be the best time
+                to go back to school for something I like doing. Raised in Utah, I had made the decision to move to a small town in Colorado in 2005 and thats where I met my husband and where we started a family. We lived there off and on for 12 years
+                while moving around to other places like San Tan Valley, AZ and Glendora, CA. We moved to Utah in 2017 and we love it here so there are no plans to move anywhere else. I love hiking, camping, being outside and I have a passion for photography,
+                puzzles, crafting, black and white photos, and spending quality time with my family.</p>
+        </div>
+    )
 }
+export default AboutMe;
